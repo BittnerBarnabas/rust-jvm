451348917c9247a52eb8ec0dd61a51e980d76072
barnab <bittnerbarni@gmail.com>
2020-04-25 14:46:03 -0400

    Added all parts of Class format into Klass & added some required attributes to be able to parse the test file

--- src/core/class_parser.rs
+++ src/core/class_parser.rs
@@ -1,11 +1,12 @@
-use std::io::{Cursor, Error, ErrorKind};
+use std::io::{Cursor, Error, ErrorKind, Read};
 
 use byteorder::{BigEndian, ReadBytesExt};
 
 use utils::ResultIterator;
 
 use crate::core::constant_pool::{ConstantPool, CpInfo};
-use crate::core::jvm::{AccessFlag, FieldInfo};
+use crate::core::jvm::AttributeInfo::{LineNumberTable, SourceFile};
+use crate::core::jvm::{AccessFlag, ExceptionHandler, FieldInfo, LineNumber, MethodInfo};
 use crate::core::jvm::{AttributeInfo, Klass};
 
 const CLASS_MAGIC_NUMBER: u32 = 0xCAFEBABE;
@@ -69,6 +70,9 @@ impl ClassParserImpl {
             .expect("this_class is not found!");
         let super_class = self.parse_class_pointer()?;
         let interfaces = self.parse_interfaces()?;
+        let fields = self.parse_fields()?;
+        let methods = self.parse_methods()?;
+        let attributes = self.parse_attributes()?;
 
         Ok(Klass {
             minor_version: self.minor_version,
@@ -78,6 +82,9 @@ impl ClassParserImpl {
             this_class,
             super_class,
             interfaces,
+            fields,
+            methods,
+            attributes,
         })
     }
 
@@ -130,6 +137,28 @@ impl ClassParserImpl {
         });
     }
 
+    fn parse_methods(&mut self) -> Result<Vec<MethodInfo>, Error> {
+        let mut methods: Vec<MethodInfo> = Vec::new();
+        let method_count = self.cursor.read_u16::<BigEndian>()?;
+        for _i in 0..method_count {
+            methods.push(self.parse_method()?);
+        }
+        return Ok(methods);
+    }
+
+    fn parse_method(&mut self) -> Result<MethodInfo, Error> {
+        let access_flags = self.cursor.read_u16::<BigEndian>()?;
+        let name_index = self.cursor.read_u16::<BigEndian>()?;
+        let descriptor_index = self.cursor.read_u16::<BigEndian>()?;
+        let attributes = self.parse_attributes()?;
+        return Ok(MethodInfo {
+            access_flags,
+            name_index,
+            descriptor_index,
+            attributes,
+        });
+    }
+
     fn parse_attributes(&mut self) -> Result<Vec<AttributeInfo>, Error> {
         let attributes_count = self.cursor.read_u16::<BigEndian>()?;
 
@@ -142,6 +171,76 @@ impl ClassParserImpl {
 
     fn parse_attribute(&mut self) -> Result<AttributeInfo, Error> {
         let name_index = self.cursor.read_u16::<BigEndian>()?;
-        return Ok(AttributeInfo::ConstantValue {});
+        let attribute_name;
+        if let CpInfo::Utf8 { string: str } = self.constant_pool.get(name_index as usize) {
+            attribute_name = str;
+        } else {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!(
+                    "Attribute name_index {} should point to UTF-8 in constant pool!",
+                    name_index
+                ),
+            ));
+        }
+        let attribute_length = self.cursor.read_u32::<BigEndian>()?;
+
+        return match attribute_name.as_str() {
+            "ConstantValue" => Ok(AttributeInfo::ConstantValue {
+                constant_value_index: self.cursor.read_u16::<BigEndian>()?,
+            }),
+            "Code" => {
+                let max_stack = self.cursor.read_u16::<BigEndian>()?;
+                let max_locals = self.cursor.read_u16::<BigEndian>()?;
+
+                let code_length = self.cursor.read_u32::<BigEndian>()?;
+                let code = (0..code_length)
+                    .map(|_| self.cursor.read_u8())
+                    .collect_to_result()?;
+
+                let exception_table_length = self.cursor.read_u16::<BigEndian>()?;
+
+                let exception_table = (0..exception_table_length)
+                    .map(|_| -> Result<ExceptionHandler, Error> {
+                        Ok(ExceptionHandler {
+                            start_pc: self.cursor.read_u16::<BigEndian>()?,
+                            end_pc: self.cursor.read_u16::<BigEndian>()?,
+                            handler_pc: self.cursor.read_u16::<BigEndian>()?,
+                            catch_type: self.cursor.read_u16::<BigEndian>()?,
+                        })
+                    })
+                    .collect_to_result()?;
+
+                let attributes = self.parse_attributes()?;
+
+                Ok(AttributeInfo::Code {
+                    max_stack,
+                    max_locals,
+                    code,
+                    exception_table,
+                    attributes,
+                })
+            }
+            "LineNumberTable" => {
+                let table_length = self.cursor.read_u16::<BigEndian>()?;
+                let line_number_table = (0..table_length)
+                    .map(|_| -> Result<LineNumber, Error> {
+                        Ok(LineNumber {
+                            start_pc: self.cursor.read_u16::<BigEndian>()?,
+                            line_number: self.cursor.read_u16::<BigEndian>()?,
+                        })
+                    })
+                    .collect_to_result()?;
+
+                Ok(LineNumberTable { line_number_table })
+            }
+            "SourceFile" => Ok(SourceFile {
+                sourcefile_index: self.cursor.read_u16::<BigEndian>()?,
+            }),
+            _ => Err(Error::new(
+                ErrorKind::Other,
+                format!("Unknown attribute type: {}", attribute_name),
+            )),
+        };
     }
 }
--- src/core/jvm.rs
+++ src/core/jvm.rs
@@ -98,8 +98,35 @@ impl AccessFlag {
     }
 }
 
+pub struct ExceptionHandler {
+    pub start_pc: u16,
+    pub end_pc: u16,
+    pub handler_pc: u16,
+    pub catch_type: u16,
+}
+
+pub struct LineNumber {
+    pub start_pc: u16,
+    pub line_number: u16,
+}
+
 pub enum AttributeInfo {
-    ConstantValue {},
+    ConstantValue {
+        constant_value_index: u16,
+    },
+    Code {
+        max_stack: u16,
+        max_locals: u16,
+        code: Vec<u8>,
+        exception_table: Vec<ExceptionHandler>,
+        attributes: Vec<AttributeInfo>,
+    },
+    LineNumberTable {
+        line_number_table: Vec<LineNumber>,
+    },
+    SourceFile {
+        sourcefile_index: u16,
+    },
     Custom {
         attribute_name_index: u16,
         attribute_length: u32,
@@ -114,6 +141,13 @@ pub struct FieldInfo {
     pub attributes: Vec<AttributeInfo>,
 }
 
+pub struct MethodInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
 pub struct Klass {
     pub minor_version: u16,
     pub major_version: u16,
@@ -122,4 +156,7 @@ pub struct Klass {
     pub this_class: CpInfo,
     pub super_class: Option<CpInfo>,
     pub interfaces: Vec<CpInfo>,
+    pub fields: Vec<FieldInfo>,
+    pub methods: Vec<MethodInfo>,
+    pub attributes: Vec<AttributeInfo>,
 }
