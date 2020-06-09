e31c7fc40e41642b85cb8ca658be07a7181a7e75
barnab <bittnerbarni@gmail.com>
2020-06-09 13:36:12 -0400

    Removing commented out old classLoader and adding trace info

--- runtime/src/core/class_loader.rs
+++ runtime/src/core/class_loader.rs
@@ -13,7 +13,6 @@ use crate::core::klass::klass::Klass;
 use crate::core::klass::method::MethodInfo;
 use crate::core::native::native_method_repo::NativeMethodRepo;
 use crate::core::stack_frame::{JvmStackFrame, StackFrame};
-use log::info;
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::io::Error;
@@ -35,127 +34,6 @@ pub trait ClassLoader {
     fn load_and_init_class(&self, qualified_name: String) -> Result<Rc<Klass>, JvmException>;
 }
 
-// pub struct ClassLoader {
-//     lookup_table: RefCell<HashMap<ClassKey, Rc<Klass>>>,
-//     heap: Rc<JvmHeap>,
-//     native_method_repo: Rc<NativeMethodRepo>,
-// }
-//
-// impl ClassLoader {
-//     pub fn new(heap: Rc<JvmHeap>, native_method_repo: Rc<NativeMethodRepo>) -> ClassLoader {
-//         ClassLoader {
-//             lookup_table: RefCell::new(HashMap::new()),
-//             heap,
-//             native_method_repo,
-//         }
-//     }
-//
-//     pub fn get_heap(&self) -> Rc<JvmHeap> {
-//         self.heap.clone()
-//     }
-//
-//     pub fn lookup_class(&self, qualified_name: String) -> Option<Rc<Klass>> {
-//         if let Some(klass_ref) = self.lookup_table.borrow().get(&qualified_name) {
-//             return Some(Rc::clone(klass_ref));
-//         } else {
-//             None
-//         }
-//     }
-//
-//     pub fn load_class(&self, qualified_name: String) -> Result<(), JvmException> {
-//         let klass = ClassLoader::read_and_parse_class(&qualified_name)
-//             .map_err(|err| JvmException::from_string(err.to_string()))?;
-//
-//         let referenced_classes = klass.referenced_classes();
-//
-//         klass.register_natives(); //registering if there's a registerNatives method
-//
-//         self.lookup_table
-//             .borrow_mut()
-//             .insert(klass.qualified_name(), Rc::new(klass));
-//
-//         referenced_classes
-//             .iter()
-//             .map(|class_name| self.find_or_load_class(class_name.clone()))
-//             .collect_to_result()?;
-//
-//         Ok(())
-//     }
-//
-//     pub fn find_or_load_class(&self, qualified_name: String) -> Result<Rc<Klass>, JvmException> {
-//         match self.lookup_class(qualified_name.clone()) {
-//             Some(klass) => Ok(klass),
-//             _ => {
-//                 self.load_class(qualified_name.clone())?;
-//                 self.lookup_class(qualified_name.clone())
-//                     .ok_or(JvmException::new())
-//             }
-//         }
-//     }
-//
-//     pub fn load_init_class(&self, qualified_name: String) -> Result<Rc<Klass>, JvmException> {
-//         let klass = self.find_or_load_class(qualified_name)?;
-//         self.call_cl_init(klass.clone()).unwrap_or_else(|| Ok(()))?;
-//         Ok(klass)
-//     }
-//
-//     pub fn lookup_static_method(
-//         &self,
-//         qualified_name: Qualifier,
-//     ) -> Result<Rc<MethodInfo>, JvmException> {
-//         match &qualified_name {
-//             Qualifier::MethodRef {
-//                 class_name,
-//                 name: _,
-//                 descriptor: _,
-//             } => {
-//                 let klass = self.find_or_load_class(class_name.clone())?;
-//                 klass
-//                     .get_method_by_qualified_name(qualified_name)
-//                     .ok_or(JvmException::new())
-//             }
-//             _ => Err(JvmException::new()),
-//         }
-//     }
-//
-//     pub fn bootstrap(&self) -> Result<(), JvmException> {
-//         let object = self.find_or_load_class(String::from("java/lang/Object"))?;
-//
-//         self.lookup_table
-//             .borrow()
-//             .iter()
-//             .map(|(key, klass)| self.call_cl_init(klass.clone()).unwrap_or_else(|| Ok(())))
-//             .collect_to_result()?;
-//
-//         Ok(())
-//     }
-//
-//     fn call_cl_init(&self, klass: Rc<Klass>) -> Option<Result<(), JvmException>> {
-//         klass
-//             .get_method_by_name_desc("<clinit>()V".to_string())
-//             .map(|init| {
-//                 let frame = StackFrame::new(self, &klass);
-//                 frame.execute_method(init, &klass)?;
-//                 Ok(())
-//             })
-//     }
-//
-//     fn read_and_parse_class(class_name: &String) -> Result<Klass, Error> {
-//         let class_in_bytes = ClassLoader::read_from_resources(class_name)?;
-//         ClassParser::from(class_in_bytes).parse_class()
-//     }
-//
-//     fn read_from_resources(class_name: &String) -> Result<Vec<u8>, Error> {
-//         let path = ClassLoader::class_name_to_path(class_name);
-//         let absolute_path = format!("{}{}", RESOURCES_PATH, path);
-//         std::fs::read(absolute_path)
-//     }
-//
-//     fn class_name_to_path(class_name: &String) -> String {
-//         format!("{}.class", class_name)
-//     }
-// }
-
 type ClassLoaderKey = String;
 
 pub struct ResourceLocator {
@@ -164,7 +42,7 @@ pub struct ResourceLocator {
 
 impl ResourceLocator {
     pub fn new(root: String) -> ResourceLocator {
-        info!("ClassLoader initialized.");
+        log::trace!("ClassLoader initialized.");
         ResourceLocator {
             resource_root: root,
         }
@@ -173,6 +51,7 @@ impl ResourceLocator {
     fn read_from_resource(&self, class_name: &String) -> Result<Vec<u8>, Error> {
         let path = ResourceLocator::class_name_to_path(class_name);
         let absolute_path = format!("{}/{}", self.resource_root, path);
+        log::trace!("Reading absolute file: {}", absolute_path);
         std::fs::read(absolute_path)
     }
 
@@ -239,6 +118,12 @@ impl BootstrapClassLoader {
             resource_locator,
             context,
         }
+    }
+
+    pub fn bootstrap(&self) -> Result<(), JvmException> {
+        self.load_and_init_class(String::from("java/lang/Object"))?;
+
+        Ok(())
     }
 
     /// Loads a class from the bootstrap classpath or returns a `JvmException` if the class lookup
